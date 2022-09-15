0d1da057056c96b35ff51c5c4372e664568c217b
Aleff <aleff.a.s.o@gmail.com>
2022-09-14 21:49:02 -0300

    adjusting envkeys

--- db/db.js
+++ db/db.js
@@ -4,13 +4,12 @@ const dotenv = require('dotenv');
 dotenv.config({path: './.env'})
 
 const connection = new Sequelize(
-    process.env.POSTGRES_NAME, 
-    process.env.POSTGRES_USER, 
-    process.env.POSTGRES_PASSWORD,
-    {
-        host: process.env.POSTGRES_HOST,
-        dialect: process.env.POSTGRES_DIALECT,
-        timezone: process.env.POSTGRES_TIMEZONE
+    process.env.DATABASE_TABLE, 
+    process.env.DATABASE_USER, 
+    process.env.DATABASE_PASSWORD,
+    { //Database set up
+        host: process.env.DATABASE_HOST, // Whenever you want to run in a server you can replace localhost with the IP address
+        dialect: process.env.DATABASE_DIALECT,
+        timezone: process.env.DATABASE_TIMEZONE
     });
-
 module.exports = connection;
